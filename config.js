78153b73878dd76f4cfa99946bcf37370c6385b9
Mariya Skosyrskaya <merymeggs@yandex.ru>
2020-10-30 22:09:08 +0300

    поменять порт

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const { NODE_ENV } = process.env;
 
-module.exports.PORT = process.env.PORT || 30000;
+module.exports.PORT = process.env.PORT || 3000;
 
 module.exports.JWT_SECRET = NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret';
 module.exports.MONGO_URL = NODE_ENV === 'production' ? process.env.MONGO_URL : 'mongodb://localhost:27017/news-explorerdb';
