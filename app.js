93f98ec9ae8e3d4d53bf3ceddd33fe6e3150d715
Mariya Skosyrskaya <merymeggs@yandex.ru>
2020-10-30 18:29:42 +0300

    cors

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ const errorsHandler = require('./middlewares/error-handler');
 
 const corsOptions = {
   origin: [
-    'http://localhost',
-    'https://localhost',
+    'http://localhost:8080',
     'https://explorer-news.ml',
     'https://www.explorer-news.ml',
     'https://merymegg.github.io/news-explorer-frontend/',
@@ -42,13 +41,14 @@ const corsOptions = {
 const app = express();
 
 mongoose.connect(MONGO_URL, mongooseConfig);
-app.use('*', cors(corsOptions));
+
 //app.use(limiter);
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
+app.use('*', cors(corsOptions));
 app.use(routes);
 app.use(errorLogger);
 
